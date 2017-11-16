809650377c3030da64e57521cccac7fde999f22f
andregouvea <andre.gouvea@gmail.com>
2017-11-16 04:09:46 -0200

    instance id

--- src/main/java/br/com/demo/controller/FiboGenController.java
+++ src/main/java/br/com/demo/controller/FiboGenController.java
@@ -1,6 +1,11 @@
 package br.com.demo.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,30 @@ public class FiboGenController {
 	public ResponseEntity<String> status(){
 		return new ResponseEntity<String>("OK", HttpStatus.OK);
 		
+	}
+	
+	@RequestMapping(value = "/status", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<String> instanceId(){
+		try{
+			
+			StringBuilder result = new StringBuilder();
+			URL url = new URL("http://169.254.169.254/latest/meta-data/instance-id");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setConnectTimeout(1000);
+			conn.setRequestMethod("GET");
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}
+			rd.close();
+
+			return new ResponseEntity<String>(new Date().toString() + " ID do servidor: " + result.toString(), HttpStatus.OK);
+			
+		}catch (Exception ex){
+		}
+		
+		return new ResponseEntity<String>("Localhost", HttpStatus.OK);
 	}
 	
 	
